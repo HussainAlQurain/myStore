0793a6246031d5d920621c4656c13aa1dcb0ef73
unknown <hussain.qurain@outlook.com>
2023-03-28 14:03:57 +0300

    added path for success order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductsComponent } from './products/products.component';
 import { SignupComponent } from './signup/signup.component';
+import { OrderCompletionComponent } from './order-completion/order-completion.component';
 
 const routes: Routes = [
   { path: '', component: ProductsComponent },
   { path: 'register', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'success/:name/:price', component: OrderCompletionComponent },
 ];
 
 @NgModule({
